c80b3e8505436f4931b754c0aa8983540f2d51e8
kavan hada <kavanhada1903@gmail.com>
2025-07-22 14:27:16 +0530

    Add whitenoise to requirements

--- pass_project/settings.py
+++ pass_project/settings.py
@@ -55,14 +55,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'pass_project.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'raasfiesta',
+#         'USER': 'root',
+#         'PASSWORD': os.getenv('DB_PASSWORD', 'Kavan@123'),
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'raasfiesta',
-        'USER': 'root',
-        'PASSWORD': os.getenv('DB_PASSWORD', 'Kavan@123'),
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
