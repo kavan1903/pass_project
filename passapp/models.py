dac132a41f471609d8263b4c539eef9ce2fb0566
kavan hada <kavanhada1903@gmail.com>
2025-07-22 14:49:58 +0530

    🎟️ Initial commit: Invoice system for Raas Fiesta

--- passapp/models.py
+++ passapp/models.py
@@ -7,7 +7,7 @@ class Invoice(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     customer_name = models.CharField(max_length=100)
     phone = models.CharField(max_length=15)
-    email = models.EmailField()  # ✅ Add this field
+    email = models.EmailField(default=True)  # ✅ Add this field
     passes = models.IntegerField()
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     venue = models.CharField(max_length=200, default=True)
